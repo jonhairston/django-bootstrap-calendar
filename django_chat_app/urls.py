f75d59b91a2d047e92619438fe4dde68ef6df91d
jon <jonhairston@outlook.com>
2016-03-24 14:49:31 -0400

    removing app

--- django_chat_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, url, patterns
-from . import views
-
-urlpatterns = patterns('',
-	url(r'^about', views.about, name='about'),
-	url(r'^new/', views.new_room, name='new_room'),
-	url(r'^(?P<label>[\w-]{,50})/$', views.chat_room, name='chat_room'),
-
-	)
